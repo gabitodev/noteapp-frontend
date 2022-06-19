b738fa1f24780cae98d34549fdd3190f8a7e26da
gabitodev <gabitodev@gmail.com>
2022-06-19 15:14:29 -0400

    fixed height

--- src/components/PersistLogin.js
+++ src/components/PersistLogin.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 
 // Styles
 const Container = styled.div`
-  min-height: calc(100% - 4rem);
+  min-height: calc(100vh - 8rem);
   display: flex;
   flex-direction: column;
   align-items: center;
